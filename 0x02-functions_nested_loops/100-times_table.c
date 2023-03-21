436f654c80fef35b2704cd0dd346190bdadf0480
Adryad <99326661+Adryad@users.noreply.github.com>
2023-03-21 20:15:47 +0400

    Update 100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,13 +1,11 @@
 #include "main.h"
-int main(void)
+/**
+ * print_times_table - Entry point
+ * @n: input
+ * Return: Always 0 (Success)
+ */
+void print_times_table(int n)
 {
-    print_times_table(3);
-    _putchar('\n');
-    print_times_table(5);
-    _putchar('\n');
-    print_times_table(98);
-    _putchar('\n');
-    print_times_table(12);  
-    return (0);
+
+	return (n);
 }
-void print_times_table(){}
